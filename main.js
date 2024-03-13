6ac471d17a09e08966c1e549921eb194a5abafaa
VladHolovin27 <105449427+VladHolovin27@users.noreply.github.com>
2024-03-13 17:03:47 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// // 1 
+// const a = new Date();
+// console.log(a);
+
+// 2
+// const b = new Date(1989, 1, 21, 32, 11);
+// console.log(b);
+
+// // 3
+// let date = Date.parse('2012-01-26T10:54:50.435-00:00');
+// console.log(date);
+
+// 4
+
+// const c = new Date();
+// console.log(c.getDate());
+// console.log(c.getDay());
+// console.log(c.getFullYear());
+// console.log(c.getMonth());
+// console.log(c.getMinutes());
+
+// 5
+// const timer = document.querySelector('.timer');
+// const p = document.querySelector('.clockface');
+// const firstBtn = document.querySelector('.timer-btn');
+// const secondBtn = document.querySelector('.timer-btnTwo');
+
+//     const dateA = new Date();
+//     const minutes = dateA.getMinutes();
+//     const seconds = dateA.getSeconds();
+//     const mines = minutes - seconds;
+//     p.textContent = `${mines}`;
+
+// 6
+// const getTime = new Date();
+
+// console.log(getTime.getDate());
+// console.log(getTime.getTime());
+
+// 7
+// function getDate(date) {
+//     const dateA = new Date();
+//     date = dateA.getDate();
+//     return dateA.getMonth();
+// }
